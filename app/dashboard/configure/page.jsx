054d967d9ddda0c63c3d20647f7a87be70fd7ef4
primedev88 <anujk7.dev@gmail.com>
2024-05-29 19:25:47 +0530

    commit

--- app/dashboard/configure/page.jsx
+++ app/dashboard/configure/page.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client"
+
+import React, { useContext } from 'react'
 import Configure from '@/app/ui/dashboard/configure/configure'
+import { DataContext } from '@/app/context/DataContext'
 
 const ConfigurePage = () => {
+
+  const { ranStatus, coreStatus  } = useContext(DataContext);
   return (
-    <div><Configure/></div>
+    <div><Configure ranStatus={ranStatus} coreStatus={coreStatus}/></div>
   )
 }
 
--- app/ui/dashboard/configure/configure.jsx
+++ app/ui/dashboard/configure/configure.jsx
@@ -1,41 +1,51 @@
 "use client"
 
+import styles from './configure.module.css'
 import { useConfig } from '../../../context/ConfigContext';
 import { useEffect } from 'react';
+import { FaChevronRight } from "react-icons/fa";
 
-const Configure = () => {
+const Configure = ({ ranStatus, coreStatus  }) => {
     const { selectedConfig, setSelectedConfig } = useConfig();
 
     useEffect(() => {
-        if (!selectedConfig) {            
+        if (!selectedConfig) {
             setSelectedConfig('core');
         }
     }, [selectedConfig, setSelectedConfig]);
 
     return (
-        <div>
-            <div
-                onClick={() => setSelectedConfig('core')}
-                style={{
-                    padding: '20px',
-                    margin: '10px',
-                    border: selectedConfig === 'core' ? '2px solid blue' : '2px solid gray',
-                    cursor: 'pointer'
-                }}
-            >
-                Core Config
+        <div className={styles.container}>
+            <div className={styles.col1}>
+                <div className={`${styles.btn} ${selectedConfig == 'core' ? styles.selectedcore : ''}`} onClick={() => setSelectedConfig('core')}>
+                    <div className={styles.txtico}>
+                        <div className={styles.coretxt}>1. Configure Core Network</div>
+                        <div className={styles.arrow}>
+                            <FaChevronRight />
+                        </div>
+                    </div>
+                </div>
+                
+                <div className={`${styles.rbtn} ${selectedConfig == 'ran' ? styles.selectedcore : ''}`} onClick={() => setSelectedConfig('ran')}>
+                    <div className={styles.txtico}>
+                        <div className={styles.rantxt}>2. Configure 5G Radio Access Network</div>
+                        <div className={styles.arrow}>
+                            <FaChevronRight />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div
-                onClick={() => setSelectedConfig('ran')}
-                style={{
-                    padding: '20px',
-                    margin: '10px',
-                    border: selectedConfig === 'ran' ? '2px solid blue' : '2px solid gray',
-                    cursor: 'pointer'
-                }}
-            >
-                RAN Config
+            <div className={styles.col2}></div>
+            <div className={`${styles.col3} ${selectedConfig == 'core' ? styles.corecol3 : ''}`}>
+                {
+                    selectedConfig == 'core' ? (
+                        <div>Core</div>
+                    ) : (
+                        <div>Ran</div>
+                    )
+                }
             </div>
+
         </div>
     );
 };
